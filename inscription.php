6a98f52b0207f1b632147cc9d4f591e1b7c52c98
toure <tourekadiy@gmail.com>
2017-10-31 16:57:19 +0100

    test branch

--- inscription.php
+++ inscription.php
@@ -2,68 +2,108 @@
 <html>
     <head>
         <meta charset="UTF-8">
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel=“stylesheet” href=“https://necolas.github.io/normalize.css/7.0.0/normalize.css“>
+        <!-- Bootstrap3 online-->
+        <link rel=“stylesheet” href=“https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css” integrity=“sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u” crossorigin=“anonymous”>
+        <!-- Bootstrap3 theme online-->
         <title>Inscription</title>
     </head>
     <style>
               body {
-               
-                display: flex;
+               /*
+               display: flex;
                 flex-direction: column;
                 margin: 0;
                 padding: 0;
-                background-image: url("img/girl.jpg");
-                background-size: cover;
+                
+               
+               */
+               background-image: url("img/girl.jpg");
+               background-size: cover;
+                
             }
             form {
                 text-shadow: 1px 3px 14px #fff;
                 font-weight: 100px;
+               
+                margin: auto;
               
+            }
+            /*
+            #center {
+                background-color: red;
+                height: 10em;
+            }
+            
+            .center1 {
+                background-color: yellow;
+                height: 5em;
+            }
+            */
+            #age {
+                width: 230px;
             }
             
     </style>
     <body>
+        <!--
+        <div class="container-fluid col-md-3" id="center">
+            <div class="col-md-offset-4 col-md-4 col-md-offset-4 center1"></div>
+        </div>
+        -->
         <h1>Formulaire d'inscription</h1>
         <form class='form-horizontal'method="POST" action="utilisateur.php">
             <div class="form-group">
-            <label for="name" class="col-sm-2 control-label" >Votre nom:</label>
-            <div class="col-10">
-            <input id="nom"  name="idname" type="text" /><br/>
-            </div>
-            </div>
-            <div class="form-group">
-            <label for="firstname" class="col-sm-2 control-label" >Votre prenom:</label>
-            <div class="col-10">
-            <input id="prenom" name="idprenom" type="text"/><br/>
-            </div>
+                <label for="name" class="col-sm-2 control-label" >Votre nom:</label>
+                <div class="col-10">
+                    <input id="nom"  name="idname" type="text" /><br/>
+                </div>
             </div>
             
-            <label for="age" class="col -sm-2 control-label">âge: </label>
-          
-            <input id="age" name="idage" type="number" /><br/>
-           
+            <div class="form-group">
+                <label for="firstname" class="col-sm-2 control-label" >Votre prenom:</label>
+                <div class="col-10">
+                    <input id="prenom" name="idprenom" type="text"/><br/>
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label for="age" class="col-sm-2 control-label">Age: </label>
+                <div class="col-10">
+                    <input id="age" name="idage" type="number" /><br/>
+                </div>
+            </div>
+            
             <p>Votre sexe:</p>
-            <input type='radio' name='genre' value="male" id="genretwo"  />
+            <input type='radio' name='genre' value="male" id="genretwo" checked />
             <label for="genretwo"> masculin</label>
-            <input type="radio" name="genre" value="female" id="genreone" />
+            <input type="radio" name="genre" value="female" id="genreone" checked />
             <label for="genreone"> feminin </label><br/>
             
-            <label for="mail" >Votre mail: </label>
-           
-            <input id="mail" name="idmail" type="email" />
-           
-           
-            <label for="identifiant" >Votre identifiant:</label>
-           
-            <input id="identifiant"  name="ididentifiant" type="text"/><br/>
-          
-          
-            <label for="psswd" >Votre mot de passe :</label>
-          
-            <input id="psswd" name="idpsswd" type="password" /><br/>
- 
-               
-            <label for="pays"> Dans quel pays habitez-vous?</label><br />
+            <div class="form-group">
+                <label for="mail" class="col -sm-2 control-label">Votre mail: </label>
+                <div class="col-10">
+                    <input id="mail" name="idmail" type="email" />
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label for="identifiant" class="col-sm-2 control-label" >Votre identifiant:</label>
+                <div class="col-10">
+                    <input id="identifiant"  name="ididentifiant" type="text"/><br/>
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label for="psswd" class="col-sm-2 control-label">Votre mot de passe :</label>
+                <div class="col-10">
+                    <input id="psswd" name="idpsswd" type="password" /><br/>
+                </div>
+            </div>
+            
+            <div class="form-group">
+            <label for="pays" class="col-sm-2 control-label"> Dans quel pays habitez-vous?</label><br />
+            <div class="col-10">
             <select name="pays" id="idpays">
                 <option value="country">France</option>
                 <option value="country">Allemagne</option>
@@ -72,54 +112,62 @@
                 <option value="country"> Portugal</option>
                 <option value="country"> Roumanie</option>
             </select>
-              
-            <label for="region">Dans quel region habitez-vous ?</label><br />       
+            </div>
+            </div>
+            <div class="form-group">
+            <label for="region" class="col-sm-2 control-label">Dans quel region habitez-vous ?</label><br />
+            <div class="col-10">
             <select name="region" id="pays">           
-            <option value="loremipsum">AuvergneRhône-Alpes</option>           
-            <option value="loremipsum">Aquitaine</option>           
-            <option value="loremipsum">Franche Comte</option> 
-            <option value="loremipsum">Ile de France</option>
-            <option value="loremipsum">Provence Alpes Côte d'Azur</option>
-            <option value="loremipsum"> Languedoc-Roussillon</option>
-            </select> 
-                </section>
-              
-                <section>
-            <p>Quel est votre situation personnelle?</p>
-            <input type="radio" name="perso" id="idcelib" checked />
-            <label for="celib">celibataire</label>
-            <input type="radio" name="perso" id="idmarié" checked />
-            <label for="marier"> marié(e)</label>
-            <p>Quel est votre situation professionnelle?</p>
-            <input type="checkbox" name="trav"  value="chômage" id="idchomage" />
-            <label for="chmge" class='checkbox-inline'>au chômage</label>
-            <input type="checkbox"  name="trav" value="reconversion" id="idreconv" />
-            <label for="reconv" class='checkbox-inline'>en reconversion professionnelle</label>
-            <input type="checkbox" name="trav" value="actif" id="idactif" />
-            <label for="actif" class='checkbox-inline'>en profession</label> 
-                </section>
-                </div>
-                <div>
-                <section> 
-            <p> Selectionner vo(s) centre(s) d'intérêt</p>
-            <input type="checkbox" name= "interet" value="sports" id="idsports" />
-            <label for="sp">sports</label>
-            <input type="checkbox" name="interet" value="travel" id="idtravel" />
-            <label for="vg">voyages</label>
-            <input type="checkbox" name="interet" value="ct" id="idct" />
-            <label for="ctr">culture</label>
-            <input type="checkbox" name="interet" value="art" id="idart" />
-            <label for="art">arts</label>
-            <input type="checkbox" name="interet" value="crs" id="idcrs" />
-            <label for="crs">education</label> 
-            <input type="checkbox" name="interet" value="orie" id="idapp" />
-            <label for="orientation">orientation professionnelle</label>
-            <input type="checkbox" name="interet" value="gastron" id="idgastron" />
-            <label for="gastronomie">gastronomie</label>
+                <option value="loremipsum">AuvergneRhône-Alpes</option>           
+                <option value="loremipsum">Aquitaine</option>           
+                <option value="loremipsum">Franche Comte</option> 
+                <option value="loremipsum">Ile de France</option>
+                <option value="loremipsum">Provence Alpes Côte d'Azur</option>
+                <option value="loremipsum"> Languedoc-Roussillon</option>
+            </select>
+            </div>   
+            </div>
+            <section>
+                <p>Quel est votre situation personnelle?</p>
+                <input type="radio" name="perso" id="idcelib" checked />
+                <label for="celib">celibataire</label>
+                <input type="radio" name="perso" id="idmarié" checked />
+                <label for="marier"> marié(e)</label>
+                <p>Quel est votre situation professionnelle?</p>
+                <input type="checkbox" name="trav"  value="chômage" id="idchomage" />
+                <label for="chmge" class='checkbox-inline'>au chômage</label>
+                <input type="checkbox"  name="trav" value="reconversion" id="idreconv" />
+                <label for="reconv" class='checkbox-inline'>en reconversion professionnelle</label>
+                <input type="checkbox" name="trav" value="actif" id="idactif" />
+                <label for="actif" class='checkbox-inline'>en profession</label> 
             </section>
+               
+            <section>
+                <div>
+                    <p> Selectionner vo(s) centre(s) d'intérêt</p>
+                    <input type="checkbox" name= "interet" value="sports" id="idsports" />
+                    <label for="sp">sports</label>
+                    <input type="checkbox" name="interet" value="travel" id="idtravel" />
+                    <label for="vg">voyages</label>
+                    <input type="checkbox" name="interet" value="ct" id="idct" />
+                    <label for="ctr">culture</label>
+                    <input type="checkbox" name="interet" value="art" id="idart" />
+                    <label for="art">arts</label>
+                    <input type="checkbox" name="interet" value="crs" id="idcrs" />
+                    <label for="crs">education</label> 
+                    <input type="checkbox" name="interet" value="orie" id="idapp" />
+                    <label for="orientation">orientation professionnelle</label>
+                    <input type="checkbox" name="interet" value="gastron" id="idgastron" />
+                    <label for="gastronomie">gastronomie</label>
                 </div>
-                <button type="submit" class="btn btn-primary" />Register</button><br/>
-    </form>
-
+            </section>
+                
+            <button type="submit" class="btn btn-primary" />Register</button><br/>
+    </form> 
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <script src=“https://code.jquery.com/jquery-3.2.1.slim.min.js” integrity=“sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN” crossorigin=“anonymous”></script>
+   <script src=“https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js” integrity=“sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh” crossorigin=“anonymous”></script>
+   <!-- BootstrapScript3 online-->
+   <script src=“https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js” integrity=“sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa” crossorigin=“anonymous”></script>
 </body>
 </html>
--- logout.php
+++ logout.php
@@ -7,6 +7,10 @@ include_once './personne.php';
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel=“stylesheet” href=“https://necolas.github.io/normalize.css/7.0.0/normalize.css“>
+        <!-- Bootstrap3 online-->
+        <link rel=“stylesheet” href=“https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css” integrity=“sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u” crossorigin=“anonymous”>
+        <!-- Bootstrap3 theme online-->
         <title></title>
         <style>
             body {
@@ -20,7 +24,7 @@ include_once './personne.php';
     <body>
         <h1>Deconnexion</h1>
         <form method="POST" action="deconnexion.php">
-            <button name="logout"> Deconnexion </button>
+            <button type="submit" class="btn btn-primary" name="logout"> Deconnexion </button>
         </form>
     </body>
 </html>
