138933243d1bb8955b5d630377002aeec1a42e5b
toure <tourekadiy@gmail.com>
2017-11-08 17:28:02 +0100

    test branch

--- deconnexion.php
+++ deconnexion.php
@@ -6,13 +6,13 @@ $nom = filter_input(INPUT_POST, 'logout');
 
 
 
-//edrheader('location: ./logout.php');
+//edheader('location: ./logout.php');
 //session_start();
 if (isset($_POST['logout']) && isset($_SESSION['personne'])) {
     $quitter=new Database();
     $quitter->logout();
     echo "Merci d'être passé sur notre site, à bientôt!";
-    //echo "<nav><a href=\"index.php\"/>retour à l'accueil </a></nav><br/>";
+    echo "<nav><a href=\"index.php\"/>retour à l'accueil </a></nav><br/>";
 } else {
     echo "la déconnexion est déjà effective!";
     //echo "<nav><a href=\"login.php\"/>se connecter </a></nav><br/>";
